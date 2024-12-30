a458c7f6b98e8e6371814a099b6cff3d13303021
Николай Ванеев <nikolai.vaneev@gmail.com>
2024-12-30 21:24:13 +0300

    Добавлена базовая таблица контрагентов и модалка

---
+++ Views/Modals/СounterpartyDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace USProApplication.Views.Modals
+{
+    /// <summary>
+    /// Логика взаимодействия для СounterpartyDialog.xaml
+    /// </summary>
+    public partial class СounterpartyDialog : Window
+    {
+        public СounterpartyDialog()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly Regex _regex = new Regex("[^0-9]+"); // Только цифры
+
+        // Обработка ввода с клавиатуры
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = _regex.IsMatch(e.Text);
+        }
+
+        // Обработка вставки текста
+        private void TextBox_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Paste)
+            {
+                var textBox = sender as TextBox;
+                if (Clipboard.ContainsText())
+                {
+                    var clipboardText = Clipboard.GetText();
+                    e.Handled = _regex.IsMatch(clipboardText);
+                }
+            }
+        }
+    }
+}
